ccbe30ddefccfa43c35b14ad2b6f053a02597f3c
DogLooksGood <doglooksgood@gmail.com>
2024-02-05 21:13:18 +0800

    update nixConfig

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+    # substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
   };
 
   inputs = {
